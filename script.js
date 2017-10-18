974aab5703ecd27a23be4728a85c3dccae66d4d3
Antonio Ángel Morales Parejo <anzony_mp@hotmail.es>
2017-10-18 10:15:15 +0200

    Puntos Calientes

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
 LEAFLET = {
     Constructor:function(){
-        var map = L.map('map').fitWorld();
+        var map = L.map('map').setView([0, 0], 1);
+        
+        var mbAttr = 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+		'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+		'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+        mbUrl = 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw';
 
-        L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-        maxZoom: 18,
-        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
-        '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-        'Imagery © <a href="http://mapbox.com">Mapbox</a>',
-        id: 'mapbox.streets'
-        }).addTo(map);
-        map.locate({setView: true, maxZoom: 16});
+        L.tileLayer(mbUrl, {id: 'mapbox.streets', attribution: mbAttr}).addTo(map);
+        
+        map.locate({setView: true, maxZoom: 5});
         function onLocationFound(e) {
             var radius = e.accuracy / 2;
 
             L.marker(e.latlng).addTo(map)
-                .bindPopup("You are within " + radius + " meters from this point").openPopup();
+                .bindPopup("You are within " + radius + " meters from this point");
 
             L.circle(e.latlng, radius).addTo(map);
+            L.heatLayer(e.latlng, radius).addTo(map);
         }
 
         function onLocationError(e) {
